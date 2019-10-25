fb56422bb4bbad5d4714138831a69a7032ec1626
jbecherer <jbecherer@gmx.net>
2019-10-25 10:41:10 -0700

    added a couple of new figure cinfigurations

--- figure_design/figure_designs.m
+++ figure_design/figure_designs.m
@@ -4,10 +4,13 @@ function [fig, ax] = figure_designs( kind, visible)
 %     kind has following option:
 %        11s         :  (1)v x (1)h panels -  (s)quare shape
 %        14snl       :  (1)v x (4)h panels -  (s)quare shape -  (n)o separate (l)abels
+%        21hnl       :  (2)v x (1)h panels -  (h)ori.  shape -  (n)o separate (l)abels
 %        22snl       :  (2)v x (2)h panels -  (s)quare shape -  (n)o separate (l)abels
 %        22sxyl      :  (2)v x (2)h panels -  (s)quare shape -  (x) and (y) separate (l)abels
 %        23syl       :  (2)v x (3)h panels -  (s)quare shape -  (y) separate (l)abels
+%        31hnl       :  (3)v x (1)h panels -  (h)ori.  shape -  (n)o separate (l)abels
 %        34snl       :  (3)v x (4)h panels -  (s)quare shape -  (n)o separate (l)abels
+%        41hnl       :  (4)v x (1)h panels -  (h)ori.  shape -  (n)o separate (l)abels
 %        71snl       :  (7)v x (1)h panels -  (h)ori.  shape -  (n)o separate (l)abels
 %
 %
@@ -23,6 +26,15 @@ if strcmp(kind, '11s') %{{{
    fig = figure('Color',[1 1 1],'visible',visible,'Paperunits','centimeters',...
          'Papersize',[12 10]*.8,'PaperPosition',[0 0 12 10]*.8);
       [ax, ~] = create_axes(fig, 1, 1, 0);
+         squeeze_axes(ax, .95 ,.95 );
+         shift_axes(ax, 0, 0);
+
+         %}}}
+
+elseif strcmp(kind, '21hnl') %{{{
+   fig = figure('Color',[1 1 1],'visible',visible,'Paperunits','centimeters',...
+         'Papersize',[30 20]*.8,'PaperPosition',[0 0 30 20]*.8);
+      [ax, ~] = create_axes(fig, 2, 1, 0);
          squeeze_axes(ax, .95 ,.95 );
          shift_axes(ax, 0, 0);
 
@@ -77,6 +89,15 @@ elseif strcmp(kind, '23syl')  %{{{
                shift_axes(ax, 0.01, +.05);
          %}}}
 
+elseif strcmp(kind, '31hnl') %{{{
+   fig = figure('Color',[1 1 1],'visible',visible,'Paperunits','centimeters',...
+         'Papersize',[30 20]*.8,'PaperPosition',[0 0 30 20]*.8);
+      [ax, ~] = create_axes(fig, 3, 1, 0);
+         squeeze_axes(ax, .95 ,.95 );
+         shift_axes(ax, 0, 0);
+
+         %}}}
+
 elseif strcmp(kind, '34snl') %{{{
  fig = figure('Color',[1 1 1],'visible',visible,'Paperunits','centimeters',... 
          'Papersize',[42 30]*.8,'PaperPosition',[0 0 42 30]*.8);
@@ -84,6 +105,15 @@ elseif strcmp(kind, '34snl') %{{{
          [ax, ~] = create_axes(fig, 3, 4, 0);
          %squeeze_axes(ax, .9,1);
          shift_axes(ax, 0,0);
+
+         %}}}
+
+elseif strcmp(kind, '41hnl') %{{{
+   fig = figure('Color',[1 1 1],'visible',visible,'Paperunits','centimeters',...
+         'Papersize',[30 30]*.8,'PaperPosition',[0 0 30 30]*.8);
+      [ax, ~] = create_axes(fig, 4, 1, 0);
+         squeeze_axes(ax, .95 ,.95 );
+         shift_axes(ax, 0, 0);
 
          %}}}
 
