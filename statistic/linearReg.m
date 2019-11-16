790671c8b538570f6758ef260e1f2d74f28b1153
jbecherer <jbecherer@gmx.net>
2019-11-16 10:01:34 -0800

    added polyfit and just slope as options

--- statistic/linearReg.m
+++ statistic/linearReg.m
@@ -1,10 +1,10 @@
-function [lg, con_lg, r, con_r] = linearReg(x,y)
+function [lg, con_lg, r, con_r] = linearReg(x,y, regtyp)
 %% [lg, con_lg, r, con_r] = linearReg(x,y)
 %
 %  This function calculates the coefficients of a simple inear regression 
 %  between signals x,y with
 %
-%  y = lg(1)*x + lg(2);
+%  
 %
 %
 %  OUTPUT
@@ -12,14 +12,21 @@ function [lg, con_lg, r, con_r] = linearReg(x,y)
 %     con_lg   :  95% convidence intervals on coeffs
 %     r        :  correlation coeff
 %     con_r    :  convidence interval for r
+%     regtyp   : 0            : slope   y = lg* x        no off-set   
+%                1  (default) : linear   y = lg(1)*x + lg(2) 
+%                2-inf        : just using polyfit, no error on coeff con_lg = []
 %
 %
 %   created by: 
 %        Johannes Becherer
 %        Wed Nov 13 11:29:57 PST 2019
 
-% remove nans
 
+if nargin <3
+   regtyp = 1;
+end
+
+% remove nans
    ii_nnan = ~isnan(x) & ~isnan(y);
    x = x(ii_nnan);
    y = y(ii_nnan);
@@ -27,23 +34,40 @@ function [lg, con_lg, r, con_r] = linearReg(x,y)
 
    N = length(x);
 
-   % Walters Book eq. 8.6
-   lg(1) =  ( N*sum(x.*y)-sum(x)*sum(y) )/( N*sum(x.*x)- sum(x)^2 );
-   lg(2) =  ( sum(x.*x)*sum(y) - sum(x)*sum(x.*y) )/( N*sum(x.*x)-sum(x)^2  );
+   if regtyp == 1  % linear regression
 
+      % Walters Book eq. 8.6
+      lg(1) =  ( N*sum(x.*y)-sum(x)*sum(y) )/( N*sum(x.*x)- sum(x)^2 );
+      lg(2) =  ( sum(x.*x)*sum(y) - sum(x)*sum(x.*y) )/( N*sum(x.*x)-sum(x)^2  );
 
-   % eq. 8.8
-   stdy = ( 1/(N-2) * sum( (y-lg(1)*x-lg(2)).^2 ) )^.5
+      % eq. 8.8
+      stdy = ( 1/(N-2) * sum( (y-lg(1)*x-lg(2)).^2 ) )^.5
 
-   % eq 8.9 std of slope and offset
-   std_lg(1) = stdy * ( N/( N*sum(x.*x)-sum(x)^2 ) )^.5; 
-   std_lg(2) = stdy * ( sum(x.*x)/( N*sum(x.*x)-sum(x)^2 ) )^.5; 
+      % eq 8.9 std of slope and offset
+      std_lg(1) = stdy * ( N/( N*sum(x.*x)-sum(x)^2 ) )^.5; 
+      std_lg(2) = stdy * ( sum(x.*x)/( N*sum(x.*x)-sum(x)^2 ) )^.5; 
 
+      % convedence limits for 95% (only valid for N>20) for smaller sample 
+      % the pre factor 2 needs to be adjusted according to Walter table 6.1
+      con_lg(1,:) = lg(1) + 2*[-1 1]*std_lg(1);
+      con_lg(2,:) = lg(2) + 2*[-1 1]*std_lg(2);
 
-   % convedence limits for 95% (only valid for N>20) for smaller sample 
-   % the pre factor 2 needs to be adjusted according to Walter table 6.1
-   con_lg(1,:) = lg(1) + 2*[-1 1]*std_lg(1);
-   con_lg(2,:) = lg(2) + 2*[-1 1]*std_lg(2);
+   elseif regtyp == 0  % just slope
+
+      % Walters Book eq. 8.12 
+      lg    =  sum(x.*y)/sum(x.*x);
+      % eq. 8.13
+      stdy  = ( 1/(N-1) * sum( (y-lg*x).^2 ) )^.5;
+      % eq 8.14
+      std_lg = stdy/sqrt(sum(x.*x));
+      con_lg = lg + 2*[-1 1]*std_lg;
+
+   else % polyfit
+
+      lg = polyfit(x,y,regtyp);
+      con_lg = [];
+
+   end
 
       
     
